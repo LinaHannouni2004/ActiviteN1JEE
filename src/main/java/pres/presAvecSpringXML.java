1221d9441fa2c9d1532be253f45197c49cfd3ead
LinaHannouni <linahannounipro2004@gmail.com>
2025-04-13 15:15:26 +0100

    2eme commit de l'activité 1

--- src/main/java/pres/presAvecSpringXML.java
+++ src/main/java/pres/presAvecSpringXML.java
@@ -4,7 +4,7 @@ import metier.Imetier;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class presAvecSpringXML {
+public abstract class presAvecSpringXML implements Imetier {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
         Imetier metier = (Imetier) context.getBean("metier");
